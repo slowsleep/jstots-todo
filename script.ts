b07f8245457f3d56ccc4e5594323a6f64faa3634
Yana Alekseeva <98520302+slowsleep@users.noreply.github.com>
2024-03-11 17:31:11 +0300

    Update gitignore, tsconfig, scripts package.json. Edit index.html. Compile project from js to ts. Add readme

--- script.ts
+++ script.ts
@@ -1,68 +1,73 @@
-"use strict";
 // Create a "close" button and append it to each list item
-const myNodelist = document.getElementsByTagName("LI");
+const myNodelist: HTMLCollectionOf<Element> = document.getElementsByTagName("LI");
 let i;
 for (i = 0; i < myNodelist.length; i++) {
-    const span = document.createElement("SPAN");
-    const txt = document.createTextNode("\u00D7");
+    const span: HTMLElement = document.createElement("SPAN");
+    const txt: Text = document.createTextNode("\u00D7");
     span.className = "close";
     span.appendChild(txt);
     myNodelist[i].appendChild(span);
 }
+
 // Click on a close button to hide the current list item
-const closeButtons = document.getElementsByClassName("close");
+const closeButtons: HTMLCollectionOf<Element> = document.getElementsByClassName("close");
 // const i;
 for (i = 0; i < closeButtons.length; i++) {
     let closeButton = closeButtons[i];
-    closeButton.addEventListener("click", function (e) {
+    closeButton.addEventListener("click", function(e) {
         const li = closeButton.parentElement;
+
         if (li) {
             li.style.display = "none";
         }
     });
 }
+
 // Add a "checked" symbol when clicking on a list item
-const list = document.querySelector('ul');
+const list: HTMLUListElement | null = document.querySelector('ul');
 if (list) {
     list.addEventListener('click', function (ev) {
-        let target = ev.target;
+        let target: HTMLElement = <HTMLElement>ev.target;
         if (target && target.tagName === 'LI') {
             target.classList.toggle('checked');
         }
     }, false);
 }
+
 // Create a new list item when clicking on the "Add" button
-function newElement() {
-    const li = document.createElement("li");
-    const input = document.getElementById("myInput");
-    if (!input)
-        return;
+function newElement(): void {
+    const li: HTMLLIElement= document.createElement("li");
+    const input: HTMLInputElement = <HTMLInputElement>document.getElementById("myInput");
+    if (!input) return;
     const inputValue = input.value;
-    const textNode = document.createTextNode(inputValue);
+    const textNode: Text = document.createTextNode(inputValue);
     li.appendChild(textNode);
     if (inputValue === '') {
         alert("You must write something!");
-    }
-    else {
-        const ul = document.getElementById("myUL");
+    } else {
+        const ul: HTMLElement | null = document.getElementById("myUL");
         if (ul) {
             ul.appendChild(li);
         }
     }
     input.value = "";
-    const span = document.createElement("SPAN");
-    const txt = document.createTextNode("\u00D7");
+
+    const span: HTMLElement = document.createElement("SPAN");
+    const txt: Text = document.createTextNode("\u00D7");
     span.className = "close";
     span.appendChild(txt);
     li.appendChild(span);
-    span.addEventListener("click", function (e) {
+
+    span.addEventListener("click", function(e) {
         const li = span.parentElement;
+
         if (li) {
             li.style.display = "none";
         }
-    });
+    })
 }
-const addBtn = document.querySelector("#addBtn");
-addBtn === null || addBtn === void 0 ? void 0 : addBtn.addEventListener("click", function () {
+
+const addBtn: HTMLElement | null = document.querySelector("#addBtn")
+addBtn?.addEventListener("click", function() {
     newElement();
-});
+})
